ccc9a4e93748cfe0331fc311e24320df40bcba8b
Lemon <lemon@hirelemon.com>
2015-04-16 16:47:53 -0500

    Mohawk done, RECON halfway there.

--- js/hirelemon.js
+++ js/hirelemon.js
@@ -1,4 +1,12 @@
 $('a.intro').click(function() {
   $(this).parent('section').toggleClass('active');
   $(this).next('article').slideToggle(300);
+  
+  if ( $(this).parent('section').hasClass('active') ) {
+    $(this).parent('section').siblings('section.active').children('article').slideUp(300);
+    $(this).parent('section').siblings('section').removeClass('active');
+    $('html, body').animate({
+      scrollTop: $(this).next('article').offset().top - 120
+    }, 500);
+  }
 });
