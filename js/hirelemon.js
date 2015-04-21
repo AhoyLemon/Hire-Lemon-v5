5d53730496d66fba98499192d7fc54729207e287
Lemon <lemon@hirelemon.com>
2015-04-20 23:15:19 -0500

    Launched project.

--- js/hirelemon.js
+++ js/hirelemon.js
@@ -1,12 +1,16 @@
+// jshint -W117
 $('a.intro').click(function() {
+  $t = $(this);
   $(this).parent('section').toggleClass('active');
-  $(this).next('article').slideToggle(300);
+  $(this).next('article').slideToggle(500);
   
   if ( $(this).parent('section').hasClass('active') ) {
-    $(this).parent('section').siblings('section.active').children('article').slideUp(300);
+    $(this).parent('section').siblings('section.active').children('article').slideUp(500);
     $(this).parent('section').siblings('section').removeClass('active');
-    $('html, body').animate({
-      scrollTop: $(this).next('article').offset().top - 120
-    }, 500);
+    setTimeout(function(){ 
+      $('html, body').animate({
+        scrollTop: $t.next('article').offset().top
+      }, 300);
+    }, 502);
   }
 });
